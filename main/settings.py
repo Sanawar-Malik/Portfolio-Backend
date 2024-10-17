6ebce680736ff9467d1aa745b46ee23cb166a283
linux <sanawar78m@gmail.com>
2024-10-17 22:38:43 +0500

    Email access error solve

--- main/settings.py
+++ main/settings.py
@@ -151,8 +151,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
+EEMAIL_HOST_USER = config('EMAIL_USER', default='default_email@example.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASS', default='default_password')
 EMAIL_USE_TLS = True
 
 REST_FRAMEWORK = {
