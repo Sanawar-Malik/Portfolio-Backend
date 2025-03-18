076e959e41c888ca08d5aea69eb1cf3544ecb1b4
Sanawar-Malik <85043094+Sanawar-Malik@users.noreply.github.com>
2025-03-18 12:55:08 +0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -159,7 +159,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_URL').split('@')[-1],
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
